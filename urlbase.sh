32b18171e5b1c65c753a42f3f8750eb9623bedd1
刘兴龙 <1351024799@qq.com>
2017-04-21 10:23:58 +0800

    Update urlbase.sh

--- urlbase.sh
+++ urlbase.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
-tail -n $1 $2 | perl -pe 's/\%(\w\w)/chr hex $1/ge' | grep -o 'data\\x22:\\x22[A-Za-z0-9+/]\{2,\}=\{0,2\}' | awk '{print substr($1,14)}' | base64 -d   #/app/createOrder
+tail -n $1 $2 | grep /app/createOrder | perl -pe 's/\%(\w\w)/chr hex $1/ge' | grep -o 'data\\x22:\\x22[A-Za-z0-9+/]\{2,\}=\{0,2\}' | awk '{print substr($1,14)}' | base64 -d   #/app/createOrder
 
-tail -n $1 $2 | perl -pe 's/\%(\w\w)/chr hex $1/ge' | grep -o 'data=[A-Za-z0-9+/]\{2,\}=\{0,2\}'| awk '{print substr($1,6)}'| base64 -d  #/payCallback/appleCallback
+tail -n $1 $2 | grep /payCallback/appleCallback | perl -pe 's/\%(\w\w)/chr hex $1/ge' | grep -o 'data=[A-Za-z0-9+/]\{2,\}=\{0,2\}'| awk '{print substr($1,6)}'| base64 -d      #/payCallback/appleCallback
 
 #urldecode解码   perl -pe 's/\%(\w\w)/chr hex $1/ge'
 #提取data数据    grep -o 'data\\x22:\\x22[A-Za-z0-9+/]\{2,\}=\{0,2\}' | awk '{print substr($1,14)}'
